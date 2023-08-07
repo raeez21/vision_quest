9240f2dc94209f09f789845680ff4780344756da
ajayjoseaji <ajayjose350@gmail.com>
2023-08-08 02:10:05 +0530

    home route

---
+++ frontend_vision_quest/src/app/home/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1>Hello, Home pageeee!</h1>
+        </div>
+      </>
+    )
+  }
