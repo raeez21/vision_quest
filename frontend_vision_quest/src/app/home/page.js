b98099f65fb3f89d49df38687184bb6721d42c9a
ajayjoseaji <ajayjose350@gmail.com>
2023-08-09 01:01:00 +0530

    footer

--- frontend_vision_quest/src/app/home/page.js
+++ frontend_vision_quest/src/app/home/page.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
 
 export default function Page() {
     return (
@@ -23,6 +23,7 @@ export default function Page() {
             </div>
             {/* Rest of your home page content */}
           </main>
+          <Footer />
         </div>
 
       </>
