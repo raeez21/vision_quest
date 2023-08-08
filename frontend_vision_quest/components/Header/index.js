f606d91d24301cc08f36fb2336e86d35c1a5bc6a
ajayjoseaji <ajayjose350@gmail.com>
2023-08-08 22:59:56 +0530

    home page initial

--- frontend_vision_quest/components/Header/index.js
+++ frontend_vision_quest/components/Header/index.js
@@ -5,10 +5,20 @@ export default function Header() {
       <>
         <header className="bg-gray-800 text-white py-4">
           <nav className="container mx-auto flex justify-between items-center">
-            <Link href="/">Vision Quest</Link>
+            <Link className="text-4xl font-bold text-white-700" href="/">Vision Quest</Link>
               <div className="space-x-4">
-                <Link href="/signin">Sign In</Link>
-                <Link href="/signup">Sign Up</Link>
+                <Link 
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50" 
+                    href="/signin"
+                >
+                        Sign In
+                </Link>
+                <Link 
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white-900 hover:bg-gray-50" 
+                    href="/signin"
+                >
+                        Sign Up
+                </Link>
               </div>
           </nav>
         </header>
--- frontend_vision_quest/src/app/home/page.js
+++ frontend_vision_quest/src/app/home/page.js
@@ -4,10 +4,27 @@ import Header from "../../../components/Header";
 export default function Page() {
     return (
       <>
-        <Header />
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <h1>Hello, Home pageeee!</h1>
+        <div>
+          <Header />
+          <main className="container mx-auto mt-8">
+            <div className="flex items-center mb-8 bg-gray-800 p-10 rounded-2xl space-x-4 ">
+              <div className="flex-1">
+                <h2 className="text-4xl font-bold text-gray-600 font-Poppins">Vision Quest</h2>
+                <p className="text-gray-600 mt-2">
+                  Explore, Detect and Discover products with our Cutting-Edge Visionary Web App!
+                  Simply upload an image of any product, and our advanced algorithms will swiftly identify its category, enabling you to explore a world of possibilities. From basic object recognition to advanced product matching, our user-friendly interface effortlessly connects you with the items you desire. Elevate your shopping journey with our revolutionary web app, and embark on a visual quest like never before!
+                </p>
+              </div>
+              <div className="flex-1 flex items-center justify-center">
+                <button className="font-bold bg-slate-500 text-slate-900 px-4 py-2 rounded">
+                  Try Now
+                </button>
+              </div>
+            </div>
+            {/* Rest of your home page content */}
+          </main>
         </div>
+
       </>
     )
   }
