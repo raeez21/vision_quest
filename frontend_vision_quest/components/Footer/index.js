6d6e059119baface31e341d4417e55ebec52fe75
ajayjoseaji <ajayjose350@gmail.com>
2023-08-09 01:48:32 +0530

    homepage UI

--- frontend_vision_quest/components/Footer/index.js
+++ frontend_vision_quest/components/Footer/index.js
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
       <>
-        <footer className="bg-gray-800 text-white py-4 absolute bottom-0 w-full">
+        <footer className="bg-gray-800 text-white py-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div>
                     <h2 className="text-4xl font-bold text-white-600">Vision Quest</h2>
--- frontend_vision_quest/src/app/home/page.js
+++ frontend_vision_quest/src/app/home/page.js
@@ -1,12 +1,20 @@
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
 
+const reviewImages = [
+  'image_url_1.jpg',
+  'image_url_2.jpg',
+  'image_url_3.jpg',
+  'image_url_4.jpg',
+  'image_url_5.jpg',
+];
+
 export default function Page() {
     return (
       <>
-        <div>
+        <div className="flex flex-col h-screen justify-between">
           <Header />
-          <main className="container mx-auto mt-8">
+          <main className="container mb-auto mx-auto mt-8">
             <div className="flex items-center mb-8 bg-gray-800 p-10 rounded-2xl space-x-4 ">
               <div className="flex-1">
                 <h2 className="text-4xl font-bold text-gray-600 font-Poppins">Vision Quest</h2>
@@ -21,7 +29,23 @@ export default function Page() {
                 </button>
               </div>
             </div>
-            {/* Rest of your home page content */}
+
+            <div className="mb-8">
+              <h2 className="text-3xl font-semibold mb-4">Reviews</h2>
+              <div className="flex space-x-4 justify-between">
+                <div>
+                  
+                </div>
+                {reviewImages.map((imageUrl, index) => (
+                  <div
+                    key={index}
+                    className="flex-none w-56 h-48 bg-gray-300 rounded-xl"
+                    style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover' }}
+                  ></div>
+                ))}
+              </div>
+            </div>
+
           </main>
           <Footer />
         </div>
