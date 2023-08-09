2f362049061ea449deeaa36251bacfdc062c0a7c
ajayjoseaji <ajayjose350@gmail.com>
2023-08-09 14:07:52 +0530

    image name in dashboard

--- frontend_vision_quest/src/app/dashboard/page.js
+++ frontend_vision_quest/src/app/dashboard/page.js
@@ -2,12 +2,12 @@ import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
 
 const imageUrls = [
-    'image_url_1.jpg',
-    'image_url_2.jpg',
-    'image_url_3.jpg',
-    'image_url_4.jpg',
-    'image_url_5.jpg',
-];
+    { url: 'image_url_1.jpg', caption: 'Image 1' },
+    { url: 'image_url_2.jpg', caption: 'Image 2' },
+    { url: 'image_url_3.jpg', caption: 'Image 3' },
+    { url: 'image_url_4.jpg', caption: 'Image 4' },
+    { url: 'image_url_5.jpg', caption: 'Image 5' },
+  ];
 
 export default function Page() {
     return (
@@ -39,27 +39,41 @@ export default function Page() {
                     <div className="pt-10 flex flex-col items-start space-y-6">
                         <div>
                             <h3 className="text-2xl font-semibold mb-2">Last Week</h3>
-                            <div className="flex space-x-4">
-                                {/* Image Divs */}
-                                {imageUrls.map((imageUrl, index) => (
+                            <div className="flex space-x-4 items-center">
+                                {imageUrls.map((image, index) => (
                                     <div
-                                    key={index}
-                                    className="flex-none w-56 h-48 bg-gray-300 rounded-xl"
-                                    style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover' }}
-                                  ></div>
+                                        key={index}
+                                        className="flex flex-col items-center"
+                                    >
+                                        <div
+                                            className="flex-none w-56 h-48 bg-gray-300 rounded-xl"
+                                            style={{
+                                                backgroundImage: `url(${image.url})`,
+                                                backgroundSize: 'cover',
+                                            }}
+                                        ></div>
+                                        <p className="mt-2 text-sm">{image.caption}</p>
+                                    </div>
                                 ))}
                             </div>
                         </div>
                         <div>
                             <h3 className="text-2xl font-semibold mb-2">Last Month</h3>
-                            <div className="flex space-x-4">
-                                {/* Image Divs */}
-                                {imageUrls.map((imageUrl, index) => (
+                            <div className="flex space-x-4 items-center">
+                                {imageUrls.map((image, index) => (
                                     <div
-                                    key={index}
-                                    className="flex-none w-56 h-48 bg-gray-300 rounded-xl"
-                                    style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover' }}
-                                  ></div>
+                                        key={index}
+                                        className="flex flex-col items-center"
+                                    >
+                                        <div
+                                            className="flex-none w-56 h-48 bg-gray-300 rounded-xl"
+                                            style={{
+                                                backgroundImage: `url(${image.url})`,
+                                                backgroundSize: 'cover',
+                                            }}
+                                        ></div>
+                                        <p className="mt-2 text-sm">{image.caption}</p>
+                                    </div>
                                 ))}
                             </div>
                         </div>
