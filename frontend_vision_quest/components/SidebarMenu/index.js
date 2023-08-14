1560d75cbcec8952c307022a6073be99e51ac648
ajayjoseaji <ajayjose350@gmail.com>
2023-08-14 14:54:16 +0530

    sidebar menu

---
+++ frontend_vision_quest/components/SidebarMenu/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'next/link';
+// import { useRouter } from 'next/router';
+
+export const SidebarMenu = () => {
+    // const router = useRouter();
+
+    // const isActive = (route) => {
+    //     return router.pathname === route ? 'text-blue-500' : 'text-white';
+    // };
+    return (
+        <div>
+            <nav className="fixed left-0 top-0 bg-white h-screen w-1/4 shadow-md">
+                <div className="flex flex-col space-y-4">
+                    <div className="p-4">
+                        <Link 
+                            href="/dashboard"
+                            className="hover:text-blue-500 text-gray-700 font-bold"
+                        >
+                            Dashboard
+                        </Link>
+                    </div>
+                    <div className="p-4">
+                        <Link 
+                            href="/analyse"
+                            className="hover:text-blue-500 text-gray-700 font-bold"
+                        >
+                            Analyse
+                        </Link>
+                    </div>
+                    <div className='p-4'>
+                        <Link 
+                            href="/results"
+                            className="hover:text-blue-500 text-gray-700 font-bold"
+                        >
+                            Results
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
