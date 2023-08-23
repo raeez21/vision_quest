08d4ae94e2b4cbfab8506c03fb977b1b77e21d64
ajayjoseaji <ajayjose350@gmail.com>
2023-08-23 13:19:46 +0530

    profile page routing updates

--- frontend_vision_quest/src/app/profile/page.js
+++ frontend_vision_quest/src/app/profile/page.js
@@ -5,6 +5,7 @@ import Footer from '../../../components/Footer';
 import { useAuth } from '../../../components/AuthContext';
 import { useRouter } from 'next/navigation';
 import { SidebarMenu } from '../../../components/SidebarMenu';
+import Link from 'next/link';
 
 function Profile() {
     const { authToken, logout } = useAuth()
@@ -27,33 +28,44 @@ function Profile() {
                 <main className="container mb-auto mx-auto mt-8">
                     <div className="bg-gray-100 p-6 rounded-lg">
                         <h2 className="text-2xl font-bold mb-4">Your Profile</h2>
-                        {/* {session ? ( */}
-                        <div>
-                            <p className="mb-2">
-                                <span className="font-semibold">Username:</span> user
-                            </p>
-                            <p className="mb-2">
-                                <span className="font-semibold">Email:</span> abc@g.com
-                            </p>
-                            <p className="mb-2">
-                                <span className="font-semibold">First Name:</span> name1
-                            </p>
-                            <p>
-                                <span className="font-semibold">Last Name:</span> name2
-                            </p>
-                            {/* <p>
-                                <span className="font-semibold">Last Name:</span> {session.user.last_name}
-                            </p> */}
-                        </div>
-                        {/* ) : (
-                        <p>Please sign in to view your profile.</p>
-                        )} */}
-                        <button 
-                            onClick={handleLogout}
-                            className='mt-10 font-bold bg-slate-500 text-gray-900 px-4 py-2 rounded'
-                        >
-                            Logout
-                        </button>
+                        {authToken ? (
+                            <div>
+                                <p className="mb-2">
+                                    <span className="font-semibold">Username:</span> user
+                                </p>
+                                <p className="mb-2">
+                                    <span className="font-semibold">Email:</span> abc@g.com
+                                </p>
+                                <p className="mb-2">
+                                    <span className="font-semibold">First Name:</span> name1
+                                </p>
+                                <p>
+                                    <span className="font-semibold">Last Name:</span> name2
+                                </p>
+                                {/* <p>
+                                    <span className="font-semibold">Last Name:</span> {session.user.last_name}
+                                </p> */}
+                            </div>
+                            ) : (
+                            <p>Please sign in to view your profile.</p>
+                        )}
+                        {authToken ? (
+                            <button 
+                                onClick={handleLogout}
+                                className='mt-10 hover:bg-slate-500 font-bold hover:text-slate-300 px-8 py-3 rounded bg-slate-300 text-slate-500'
+                            >
+                                Logout
+                            </button>
+                        ):(
+                            <div className='mt-10'>
+                                <Link 
+                                    className='hover:bg-slate-500 font-bold hover:text-slate-300 px-8 py-3 rounded bg-slate-300 text-slate-500'
+                                    href='/signin'
+                                >
+                                    Signin
+                                </Link>
+                            </div>
+                        )}
                     </div>
                 </main>
                 <Footer />
