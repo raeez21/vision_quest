3b47333cd7fd75c1c4e554f979a0b06ea81312f3
ajayjoseaji <ajayjose350@gmail.com>
2023-08-31 22:04:48 +0530

    SidebarMenu updates

--- frontend_vision_quest/components/SidebarMenu/index.js
+++ frontend_vision_quest/components/SidebarMenu/index.js
@@ -7,24 +7,25 @@ import { usePathname } from 'next/navigation';
 export const SidebarMenu = () => {
     const router = usePathname();
     const isActive = (route) => {
-        return router === route ? 'bg-gray-600 text-slate-300 hover:bg-gray-400' : 'text-slate-500 hover:bg-gray-400';
+        return router === route ? 'bg-gray-600 text-slate-300 ' : 'text-slate-300 opacity-50 hover:opacity-100';
     };
     return (
-        <nav className="fixed left-0 top-0 bg-white w-1/4 shadow-md h-screen flex flex-col justify-center">
+        <nav className="fixed left-0 top-0 bg-gray-800 shadow-2xl w-1/4 h-screen flex flex-col justify-center">
             <div className="flex flex-col">
                 <Link 
                     href="/dashboard"
-                    className={`p-4 font-bold ${isActive('/dashboard')} `}>
+                    className={`p-4 font-bold ${isActive('/dashboard')} rounded-lg transition duration-300`}
+                >
                         Dashboard
                 </Link>
                 <Link 
                     href="/analyse"
-                    className={`p-4 font-bold ${isActive('/analyse')} `}>
+                    className={`p-4 font-bold ${isActive('/analyse')} rounded-lg transition duration-300`}>
                         Analyse
                 </Link>
                 <Link 
                     href="/profile"
-                    className={`p-4 font-bold ${isActive('/profile')} `}>
+                    className={`p-4 font-bold ${isActive('/profile')} rounded-lg transition duration-300`}>
                         Profile
                 </Link>
             </div>
